e4b4961c6e270eedd615ed009b187f3de929e687
Hari-Prasanth3 <harihari71775@email.com>
2024-01-31 16:41:14 +0530

    Error solved in get user by id

--- Backend/middleWare/authMiddleWare.js
+++ Backend/middleWare/authMiddleWare.js
@@ -51,22 +51,27 @@ const userId = (req) => {
     }
 };
 // Get User
-const checkUser = (req,res,next) => {
+const checkUser = async(req,res,next) => {
     const user_id = userId(req)
     const id = req.params.id
     console.log(user_id, id)
-     if(id){
-        try{
-            id === user_id
-            next()
-    } catch {
-        return res.status(404).json({ 
-            message: "User ID Not Found"
-        })
-    }
-} else{
-        res.status(400).json({
-            message: "Invalid User ID"
+    try {
+        const user = await User.findById(id);
+        if(!user){
+            return res.status(404).json({
+                message: 'User Not Found'
+            })
+        }
+        if(id === user_id){
+            next();
+        } else {
+            return res.status(404).json({
+                message: "User ID Not Found"
+            })
+        }
+    } catch (error) {
+        return res.status(400).json({
+            message: 'Invalid User ID'
         })
     }
 }
